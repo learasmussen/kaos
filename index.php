a4a28b0a16d0c3e2bedf4833eadf11dd44dd06ed
lea03 <ler002@edu.zealand.dk>
2025-11-26 11:32:18 +0100

    Sat index ind fra frida

--- index.php
+++ index.php
@@ -9,20 +9,31 @@ require "settings/init.php";
 <html lang="da">
 <head>
     <meta charset="utf-8">
-    
+
     <title>Sigende titel</title>
-    
+
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
-    
+
     <link href="css/styles.css" rel="stylesheet" type="text/css">
-    
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body class="text-center p-4 bg-red">
 
+
+<p class="position-absolute top-0 start-0 m-3">
+    <a href="alder.php" class="text-decoration-none">goooo</a>
+</p>
+
+
+<div class="container vh-100 d-flex flex-column justify-content-center align-items-center text-center">
+    <h1 id="overskriftleafrida">lea og fridas partybus vær med til festen</h1>
+
+    <button class="btn btn-pink btn-lg mt-5">KOM OMBORD PÅ BUSSEN</button>
+</div>
 
 
 
